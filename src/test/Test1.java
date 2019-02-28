c1e81ba2f4fef9ecc91888a00f71fe16beee100a
Magbeth <magbethlmc@gmail.com>
2019-02-28 16:42:57 +0300

    parallel stream

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -1,7 +1,20 @@
 package test;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Test1 {
-    String str = "CREATE_OFFLINEUSER_ERROR";
-    String str1 = "CREATE_OFFLINEUSER_ERROR_2";
-    String str2 = "CREATE_OFFLINEUSER_ERROR_3";
+    static String str = "create.offlineuser.error";
+    String str1 = "create.offlineuser.error_2";
+    String str2 = "create.offlineuser.error_3";
+    public static void main(String[] args) {
+        try {
+            Files.write(Paths.get("src/main/resources/properties"), "create.offlineuser.error = User ID, Password and Online Password can not be empty. While creating Offline User".getBytes());
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+    }
 }
