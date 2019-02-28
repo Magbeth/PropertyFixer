3f9ebb8a17791622d20fd1c617e916d97035a66e
Magbeth <magbethlmc@gmail.com>
2019-02-28 15:27:33 +0300

    keys for test

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -1,7 +1,7 @@
 package test;
 
 public class Test1 {
-    String str = "create.offlineuser.error";
-    String str1 = "create.offlineuser.error.2";
-    String str2 = "create.offlineuser.error";
+    String str = "CREATE_OFFLINEUSER_ERROR";
+    String str1 = "CREATE_OFFLINEUSER_ERROR_2";
+    String str2 = "CREATE_OFFLINEUSER_ERROR_3";
 }
--- src/test/Test3.java
+++ src/test/Test3.java
@@ -1,7 +1,7 @@
 package test;
 
 public class Test3 {
-    String str = "create.offlineuser.error";
-    String str1 = "create.offlineuser.error.2";
-    String str2 = "create.offlineuser.error.3";
+    String str = "CREATE_OFFLINEUSER_ERROR";
+    String str1 = "CREATE_OFFLINEUSER_ERROR_2";
+    String str2 = "CREATE_OFFLINEUSER_ERROR_3";
 }
