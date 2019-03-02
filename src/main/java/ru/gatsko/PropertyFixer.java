6b9e7432db673083f4c76f2bdd71ec505cfc000c
Magbeth <magbethlmc@gmail.com>
2019-03-03 00:50:43 +0300

    refactoring

--- src/main/java/ru/gatsko/PropertyFixer.java
+++ src/main/java/ru/gatsko/PropertyFixer.java
@@ -4,14 +4,15 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class PropertyFixer {
-    private static List<String> keys = new ArrayList<>();
+    //There is no sense to save non-unique keys, so I replaced List with Set
+    private static Set<String> keys = new HashSet<>();
 
     //translates keys to lowercase with dot-separator
     private static String fixKey(String k) {
@@ -26,19 +27,27 @@ public class PropertyFixer {
     }
 
     //reads property files, search and save keys and replace them by calling fixKey method
+    //I removed replacing keys in property file to allow the programm to be reused.
+    //If replace keys in original file when adding new .java file to this utility with non-replaced keys it won't work
+    //because property file after replacing may not content key which is necessary for programm logic to replace key in java-files/
+    //Another way if it is necessary to save keys on disk is to write them in separate file, but I believe there is no necessity
     private static void fixProperty(String path) {
         Path propertyPath = Paths.get(path);
 
         try (BufferedReader reader = Files.newBufferedReader(propertyPath)) {
-            String content = new String(Files.readAllBytes(propertyPath));
+//            String content = new String(Files.readAllBytes(propertyPath));
+            Stream<String> lines = reader.lines();
+            lines
+                    .parallel()
+                    .map(line -> line.split("=")[0].trim())
+                    .forEach(line -> keys.add(line));
+//            for (String line; (line = reader.readLine()) != null;) {
+//                String key = line.split("=")[0].trim();
+//                keys.add(key);
+//                content = replaceKey(content, key);
+//            }
 
-            for (String line; (line = reader.readLine()) != null;) {
-                String key = line.split("=")[0].trim();
-                keys.add(key);
-                content = replaceKey(content, key);
-            }
-
-            Files.write(propertyPath, content.getBytes());
+//            Files.write(Paths.get("src/main/resources/keys"), content.getBytes());
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/test/Test1.java
+++ src/test/Test1.java
@@ -1,7 +1,8 @@
 package test;
 
+
 public class Test1 {
-    String str = "CREATE_OFFLINEUSER_ERROR";
+    String str = "CREATE_OFFLINEUSER_ERROR CREATE_OFFLINEUSER_ERROR_2";
     String str1 = "CREATE_OFFLINEUSER_ERROR_2";
-    String str2 = "CREATE_OFFLINEUSER_ERROR_3";
+    String str2 = "create.offlineuser.error.3";
 }
